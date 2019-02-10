e4585ec92a2c423ed4c68a5be302208756191d7f
ADILET <alpha.z91@mail.ru>
2019-02-10 22:29:24 +0600

    Добавьте файлы проекта.

---
+++ Hostel/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel
+{
+    class Program
+    {
+        const int MIN_SALARY = 20000;
+        static void Main(string[] args)
+        {
+            Student first = new Student("Ben Hunter", "E122", 4.5, 36000, Gender.Male, EducationForm.Internal);
+            Student second = new Student("Tom Teddy", "T212", 4.0, 28000, Gender.Male, EducationForm.Internal);
+            Student third = new Student("Vicky Fox", "D105", 4.55, 45000, Gender.Female, EducationForm.Internal);
+            Student[] students = new Student[]
+            {
+                first, second, third
+            };
+        }
+        static void GetHostel(Student[] students)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                 
+            }
+        }
+    }
+}/*Общежитие в первую очередь предоставляется тем, у кого доход на члена семьи меньше 
+двух минимальных зарплат, 
+затем остальным в порядке уменьшения среднего балла. 
+Вывести список очередности предоставления мест в общежитии.*/
---
+++ Hostel/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public string Group { get; set; }
+        public double AverageGrade { get; set; }
+        public int Profit { get; set; }
+        public Gender Gender { get; set; }
+        public EducationForm Form { get; set; }
+
+        public Student(string fullName, string group, double avgGrade, int profit, Gender gender, EducationForm form)
+        {
+            FullName = fullName;
+            Group = group;
+            AverageGrade = avgGrade;
+            Profit = profit;
+            Gender = gender;
+            Form = form;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Полное имя: " + FullName +
+                "\nГруппа: " + Group +
+                "\nСредняя оценка: " + AverageGrade +
+                "\nДоход: " + Profit +
+                "\nПол: " + Gender +
+                "\nФорма обучения: " + Form + "\n");
+        }
+    }
+}
